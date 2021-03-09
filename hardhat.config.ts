d06be7972bf9ae67192e567f9e3b5c2a17c9096b
Mick de Graaf <mick@mickdegraaf.nl>
2021-03-09 13:44:37 +0100

    WIP

---
+++ hardhat.config.ts
@@ -0,0 +1,23 @@
+import "@nomiclabs/hardhat-waffle";
+import { HardhatUserConfig, task } from "hardhat/config";
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, { ethers }) => {
+  const accounts = await ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+const config: HardhatUserConfig = {
+  solidity: "0.8.1",
+};
+
