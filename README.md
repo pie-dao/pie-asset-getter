2ff9c31d34442b4b3a4939c7d5950d77e9a6b611
antodp <antoniodellaporta.13@protonmail.com>
2021-06-03 17:17:06 +0200

    Cleanup

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # pie-asset-getter
 Gets the underlying assets and amounts from a Pie
+
+# How to run
+
+```
+$ yarn
+$ yarn compile
+$ yarn get-assets
+```
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,13 +1,16 @@
-import "@nomiclabs/hardhat-waffle";
-import { HardhatUserConfig, task } from "hardhat/config";
+import '@typechain/hardhat';
+import '@nomiclabs/hardhat-ethers';
+import '@nomiclabs/hardhat-waffle';
+import { task } from "hardhat/config";
+import env from 'hardhat';
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, { ethers }) => {
-  const accounts = await ethers.getSigners();
+task("accounts", "Prints the list of accounts", async (args, hre) => {
+  const accounts = await hre.ethers.getSigners();
 
   for (const account of accounts) {
-    console.log(account.address);
+    console.log(await account.address);
   }
 });
 
@@ -17,7 +20,23 @@ task("accounts", "Prints the list of accounts", async (taskArgs, { ethers }) => 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-const config: HardhatUserConfig = {
-  solidity: "0.8.1",
+export default {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.1"
+      },
+      {
+        version: "0.7.1"
+      }
+    ]
+  },
+  networks: {
+    hardhat: {
+      forking: {
+        url: process.env["MAINNET_FORK"] || "https://sandbox.truffleteams.com/79800802-d2cd-4262-a354-93fc82bb0467" 
+      }
+    }
+  }
 };
 
